f6020b2d609917f9d7c02b0811480933e56ae843
bya2 <byaa1972@gmail.com>
2021-07-23 15:06:12 +0900

    fix: Annotation

--- shopSite/src/main/java/alpacaCorp/shopSite/Service/ProfileServiceImpl.java
+++ shopSite/src/main/java/alpacaCorp/shopSite/Service/ProfileServiceImpl.java
@@ -3,9 +3,11 @@ package alpacaCorp.shopSite.Service;
 import alpacaCorp.shopSite.DTO.Profile;
 import alpacaCorp.shopSite.Repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProfileServiceImpl implements ProfileService {
     ProfileRepository profileRepository;
 
