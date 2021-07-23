d233b2afb3659b78763adf2082ef531cc117caf2
bya2 <byaa1972@gmail.com>
2021-07-23 16:52:39 +0900

    write: change attribute annotation

--- shopSite/src/main/java/alpacaCorp/shopSite/Controller/ProfileManageController.java
+++ shopSite/src/main/java/alpacaCorp/shopSite/Controller/ProfileManageController.java
@@ -5,6 +5,7 @@ import alpacaCorp.shopSite.Service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,22 +24,8 @@ public class ProfileManageController {
      * 새로운 프로필 추가
      */
     @PostMapping("profiles/add")
-    public String addProfile(@RequestParam("id") String id,
-                             @RequestParam("name") String name,
-                             @RequestParam("color") String color,
-                             @RequestParam("img") String img,
-                             @RequestParam("start_date") String start_date,
-                             @RequestParam("end_date") String end_date) {
-        Profile profile = new Profile();
-        profile.setId(id);
-        profile.setName(name);
-        profile.setColor(color);
-        profile.setImg(img);
-        profile.setStart_date(start_date);
-        profile.setEnd_date(end_date);
-
+    public String addProfile(@ModelAttribute("profile") Profile profile) {
         profileService.addProfile(profile);
-
         return "redirect:/";
     }
 
