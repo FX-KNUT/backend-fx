7c058cee9dc20b60f08e358ca159711569f1914c
bya2 <byaa1972@gmail.com>
2021-07-23 19:20:10 +0900

    fix: using member dto instead of using profile dto

--- shopSite/src/main/java/alpacaCorp/shopSite/Controller/ProfileManageController.java
+++ shopSite/src/main/java/alpacaCorp/shopSite/Controller/ProfileManageController.java
@@ -1,6 +1,8 @@
 package alpacaCorp.shopSite.Controller;
 
+import alpacaCorp.shopSite.DTO.Member;
 import alpacaCorp.shopSite.DTO.Profile;
+import alpacaCorp.shopSite.Service.MemberService;
 import alpacaCorp.shopSite.Service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,28 +15,29 @@ import java.util.List;
 
 @Controller
 public class ProfileManageController {
-    ProfileService profileService;
+    MemberService memberService;
 
     @Autowired
-    public ProfileManageController(ProfileService profileService) {
-        this.profileService = profileService;
+    public ProfileManageController(MemberService memberService) {
+        this.memberService = memberService;
     }
 
-    /**
-     * 새로운 프로필 추가
-     */
+    /*
+     * 새로운 프로필 추가 (쓰이지 않는다면 삭제 예정)
+
     @PostMapping("profiles/add")
     public String addProfile(@ModelAttribute("profile") Profile profile) {
         profileService.addProfile(profile);
         return "redirect:/";
     }
+    */
 
     /**
      * 프로필 리스트 매핑
      */
     @PostMapping("/profiles")
-    public List<Profile> ProfileList(Model model) {
-        List<Profile> profiles = profileService.findAll();
+    public List<Member> ProfileList(Model model) {
+        List<Member> profiles = memberService.findAll();
         model.addAttribute("profiles", profiles);
         return profiles;
     }
